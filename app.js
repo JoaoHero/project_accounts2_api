13af60e5c85617080c93a73017635b4c70592733
João vitor Silva Ramos <joao.silvaramos2013@hotmail.com>
2024-02-16 22:26:18 -0300

    adicionado comentarios no codigo

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
+// Inicializando o express
 const express = require('express');
+// Criando o app com o express
 app = express();
+// Especificando a porta sendo utilizada
 port = 8080
 
-const db = require("./db/models");
+// Importando as controllers
+loginRouter = require("./controllers/login")
 
-// localhost:8080
+app.use("/", loginRouter);
+
+// Servidor rodando na porta: localhost:8080
 app.listen(port, () => {
     console.log(`Servidor iniciado na porta: ${port}`);
 });
--- controllers/login.js
+++ controllers/login.js
@@ -1,7 +1,14 @@
+// Inicializando o express
 const express = require('express');
+// Chamando rotas do express
 const router = express.Router();
+
+// Importando a conexão com banco de dados
+const db = require("../db/models");
 
 // Criando a rota login
 router.post("/login", async(req, res) => {
-    
+    console.log("Acessou a rota post!")
 });
+
+module.exports = router;
