ddcd71dab5e884bce269d41d173758736a9028b4
João vitor Silva Ramos <joao.silvaramos2013@hotmail.com>
2024-03-06 12:37:11 -0300

    Criado a rota de pix

--- controllers/userTransactions.js
+++ controllers/userTransactions.js
@@ -70,6 +70,7 @@ router.post("/userTransactions/deposit", eAdmin, async(req, res) => {
             });
         };
 
+        // Validar se o valor informado para o pix não é um valor negativo
         if(value < 0) {
             // Parar o processamento e retornar o código de erro
             return res.status(400).json({
@@ -126,6 +127,124 @@ router.post("/userTransactions/deposit", eAdmin, async(req, res) => {
         return res.status(500).json({
             error: true,
             message: "Erro ao tentar fazer o depósito. Tente novamente mais tarde.",
+        });
+    };
+});
+
+
+// Criando rota para pix do usuário
+router.post("/userTransactions/pix", eAdmin, async(req, res) => {
+
+    try {
+        //  Obtendo o valor do pix pela requisição
+        const { value } = req.body;
+        //  Obtendo o email do usuário pela requisição
+        const { email } = req.body;
+        //  Obtendo o id do usuário pelo token
+        const userId = req.user.id;
+
+        // Validar se todos os campos foram preenchidos
+        if(!value || !email) {
+            return res.status(404).json({
+                error: true,
+                message: "Necessário preencher todos os campos!"
+            });
+        };
+
+        // Validar se o valor informado para o pix não é um valor negativo
+        if(value < 0) {
+            // Parar o processamento e retornar o código de erro
+            return res.status(400).json({
+                error: true,
+                message: "Número negativos não são permitidos!"
+            });
+        };
+
+        // Validando se o tipo do dado em value inserido é um número ou string
+        if(typeof value != "number") {
+            // Parar o processamento e retornar o código de erro
+            return res.status(400).json({
+                error: true,
+                message: "Enviar apenas números!"
+            });
+        }else {
+            // Buscando o saldo do usuário no banco de dados
+            const userBalance = await db.Users.findOne({
+                attributes: ["balance"],
+                where: { id: userId }
+            });
+
+            // Salvando o saldo do usuário em uma variável
+            const { balance } = userBalance.dataValues
+
+            // Buscando o ID e o SALDO do usuário que será enviado o pix
+            const accountPix = await db.Users.findOne({
+                attributes: ["id", "balance"],
+                where: { email: email }
+            });
+
+            // Validando se a conta do usuário que será enviado o pix foi encontrada
+            if(!accountPix) {
+                // Parar o processamento e retornar o código de erro
+                return res.status(404).json({
+                    error: true,
+                    message: "Conta não encontrada!"
+                });
+            };
+
+            // Validando se o valor inserido ao pix não é maior do que o saldo em conta
+            if(value > balance) {
+                // Parar o processamento e retornar o código de erro
+                return res.status(400).json({
+                    error: true,
+                    message: "Saldo insuficitente para completar a solicitação"
+                });
+            };
+
+            // Salvando o SALDO do usuário que será enviado o pix em uma variável
+            const accountPixBalance = accountPix.dataValues.balance;
+            // Salvando o ID do usuário que será enviado o pix em uma variável
+            const accountPixId = accountPix.dataValues.id;
+
+            // Operação do novo saldo do usuário que está solicitando a transação
+            const newBalance = balance - value;
+
+            // Operação do novo saldo para conta do usuário que irá o pix
+            const newAccountPixBalance = accountPixBalance + value;
+
+            // Realizando a atualização do novo saldo do usuário que solicitou a transação
+            let update = await db.Users.update(
+                { balance: newBalance },
+                { where: {id: userId} }
+            );
+
+            // Realizando a atualização do novo saldo para a conta do usuário que foi enviado o pix
+            let update2 = await db.Users.update(
+                { balance: newAccountPixBalance },
+                {where: {id: accountPixId }},
+            );
+
+            // Validando se teve alguma alteração na operação de update
+            if(update != 0 && update2 != 0) {
+                return res.status(200).json({
+                    error: false,
+                    message: "Pix realizado com sucesso."
+                });
+            }else {
+                // Parar o processamento e retornar o código de erro
+                return res.status(404).json({
+                    error: true,
+                    message: "Não foi possível realizar o pix."
+                });
+            };
+        };
+
+    }catch(err) {
+        console.error("Erro ao tentar fazer o pix, erro:", err);
+        // Parar o processamento e retornar o código de erro
+        return res.status(500).json({
+            error: true,
+            message: "Erro ao tentar fazer o pix. Tente novamente mais tarde.",
         });
     };
 });
